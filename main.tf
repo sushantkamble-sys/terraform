25c7c756a3a28f9644238be69c592bcb7d6d3b00
sushant <sushantkamble@gmail.com>
2022-05-04 10:54:04 +0530

    main.tf add

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0c4f7023847b90238"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-082db47b199e5f302"]
+  key_name = "jenkins"
+  tags = {
+    Name = "jenkins"
+  }
+
+}
