c3603ba7601fe6bc311a9b95f4a405a7fbd1cdf5
sushant <sushantkamble@gmail.com>
2022-05-04 11:48:12 +0530

    136

--- main.tf
+++ main.tf
@@ -20,9 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  security_groups = "myjen"
-  key_name = "jenkins"
+  instance_type = "t2.micro"
   tags = {
     Name = "jenkins"
   }
