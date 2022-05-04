9b583b0ac9bfcbd1bd87e8dc98423286f7dcc4f1
sushant <sushantkamble@gmail.com>
2022-05-04 11:21:49 +0530

    147

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami = "data.aws_ami.jenkins.id"
+  ami = "ami-09d56f8956ab235b3"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-082db47b199e5f302"]
   key_name = "jenkins"
