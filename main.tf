3e43d32a883df97a0a54858e97a416691c07bb52
sushant <sushantkamble@gmail.com>
2022-05-04 11:29:19 +0530

    145

--- main.tf
+++ main.tf
@@ -1,10 +1,26 @@
 provider "aws" {
   region = "us-west-1"
 }
+#-----Ec2 Instance------
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 resource "aws_instance" "web" {
-  ami = "ami-0022f774911c1d690"
-  instance_type = "t2.micro"
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-082db47b199e5f302"]
   key_name = "jenkins"
   tags = {
